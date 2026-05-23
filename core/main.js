46a478f4d7610e2ac1877f48367213eb237aa577
nullbit26 <nullbit26@github.com>
2026-05-23 23:47:46 +0200

    fix: minWidth/minHeight locked to default 1240x800

--- core/main.js
+++ core/main.js
@@ -66,8 +66,8 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1240,
     height: 800,
-    minWidth: 1100,
-    minHeight: 720,
+    minWidth: 1240,
+    minHeight: 800,
     frame: false,
     transparent: false,
     backgroundColor: '#1c1c1e',
