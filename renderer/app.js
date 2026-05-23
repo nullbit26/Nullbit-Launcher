9692a18dc5f38bd6d96b27cc956c5efddde7982c
nullbit26 <nullbit26@github.com>
2026-05-24 00:20:22 +0200

    ux: revert shutdown to 0.6s - syncs with other stop animations

--- renderer/app.js
+++ renderer/app.js
@@ -1060,7 +1060,7 @@ function _offlineStatusBar() {
   setTimeout(() => {
     bar.classList.remove('bot-shutdown');
     bar.classList.add('bot-offline');
-  }, 1250);
+  }, 650);
 }
 
 function _bootStatusBar() {
